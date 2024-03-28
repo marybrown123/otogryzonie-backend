429561dfb8ebb15ad3732845bd3bc9038d117e2e
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 12:54:29 +0100

    add prisma service in user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { UserController } from 'src/user/user.controller';
 import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [],
-  providers: [UserService],
+  providers: [UserService, PrismaService],
   exports: [UserService],
   controllers: [UserController],
 })
