1faba8d55d80b7a485e38c360144e755231af88e
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 14:17:44 +0100

    add generate authentication token method in token service

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TokenService } from 'src/token/token.service';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
   providers: [TokenService],
-  exports: [TokenService],
+  exports: [TokenService, JwtModule],
   controllers: [],
 })
 export class TokenModule {}
--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { AuthenticationTokenDTO } from 'src/token/DTOs/authentication-token.dto';
+import { AuthenticatedUserPayload } from 'src/token/interfaces/authenticated-user-payload.interface';
 
 @Injectable()
 export class TokenService {
-  constructor() {}
+  constructor(private jwtService: JwtService) {}
+
+  async generateAuthenticationToken(
+    user: User,
+  ): Promise<AuthenticationTokenDTO> {
+    const payload: AuthenticatedUserPayload = {
+      email: user.email,
+      sub: user.id,
+    };
+
+    const authenticationToken = this.jwtService.sign(payload);
+
+    return { authenticationToken };
+  }
 }
