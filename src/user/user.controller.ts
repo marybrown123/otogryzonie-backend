c1c941e9b55560cb10dc9c9efe8d6c4a8c9ed792
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 13:16:59 +0100

    add create user method in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from 'src/user/DTOs/create-user.dto';
+import { UserResponse } from 'src/user/responses/user.response';
+import { UserService } from 'src/user/user.service';
 
 @Controller('/user')
 export class UserController {
-  constructor() {}
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() user: CreateUserDTO): Promise<UserResponse> {
+    return this.userService.createUser(user);
+  }
 }
