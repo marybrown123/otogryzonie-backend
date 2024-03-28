9a1d20b3001f75a5f767cfa318cf10d53cf03a4a
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 12:29:52 +0100

    add class validation

--- src/user/DTOs/create-user.dto.ts
+++ src/user/DTOs/create-user.dto.ts
@@ -1,10 +1,31 @@
 import { UserType } from '@prisma/client';
+import {
+  IsEmail,
+  IsPhoneNumber,
+  IsString,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
 
 export class CreateUserDTO {
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(8)
   password: string;
+
+  @IsString()
   name: string;
-  secondNAme: string;
+
+  @IsString()
+  secondName: string;
+
+  @IsString()
+  @IsPhoneNumber()
   phoneNumber: string;
+
+  @ValidateNested()
   type: UserType;
 }
