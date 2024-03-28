07569ac903b38dffa1a2735974cfbc7f856bc26a
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 20:48:36 +0100

    add swagger to user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateUserDTO } from 'src/user/DTOs/create-user.dto';
 import { UserResponse } from 'src/user/responses/user.response';
 import { UserService } from 'src/user/user.service';
 
+@ApiTags('user')
 @Controller('/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Sign up - Create new user' })
+  @ApiCreatedResponse({ type: UserResponse })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   async createUser(@Body() user: CreateUserDTO): Promise<UserResponse> {
     return this.userService.createUser(user);
   }
