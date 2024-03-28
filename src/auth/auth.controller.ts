7d6965e65302adf36b976d80bb9351d3173c7d27
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 18:32:20 +0100

    add auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { AuthenticationTokenDTO } from 'src/token/DTOs/authentication-token.dto';
+import { TokenService } from 'src/token/token.service';
+import { CurrentUser } from 'src/user/decorators/current-user.decorator';
 
 @Controller('/auth')
 export class AuthController {
-  constructor() {}
+  constructor(private readonly tokenServcie: TokenService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post()
+  async login(@CurrentUser() user: User): Promise<AuthenticationTokenDTO> {
+    return this.tokenServcie.generateAuthenticationToken(user);
+  }
 }
