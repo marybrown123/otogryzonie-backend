86ff13248edbc7766a556c0bbecf3ba3d8c38b35
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 13:57:09 +0100

    add validate user method in auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [],
-  providers: [AuthService],
+  providers: [AuthService, UserService],
   controllers: [AuthController],
   exports: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string): Promise<User> {
+    const userFromDb = await this.userService.findUserByEmail(email);
+
+    if (!userFromDb) {
+      throw new HttpException('Incorrect credentials', HttpStatus.UNAUTHORIZED);
+    }
+
+    const passwordValid = await bcrypt.compare(password, userFromDb.password);
+
+    if (!passwordValid) {
+      throw new HttpException('Incorrect credentials', HttpStatus.UNAUTHORIZED);
+    }
+  }
 }
