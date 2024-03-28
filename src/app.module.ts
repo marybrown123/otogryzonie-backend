e78db7574bc03461e2e5ed7f2baed59f53d9bfd4
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 20:14:56 +0100

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/auth/auth.module';
 import { TokenModule } from 'src/token/token.module';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [UserModule, AuthModule, TokenModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    TokenModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
