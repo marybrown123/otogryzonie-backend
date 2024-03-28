dbfcebbeec480ec3e910cb2db75562b92e7ddf86
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 18:27:10 +0100

    add jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
+import { AuthenticatedUserPayload } from 'src/token/interfaces/authenticated-user-payload.interface';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECREET,
+    });
+  }
+
+  async validateUser(payload: AuthenticatedUserPayload): Promise<User> {
+    const currentUser = await this.userService.findUserById(payload.sub);
+
+    if (currentUser) {
+      return currentUser;
+    }
+  }
+}
