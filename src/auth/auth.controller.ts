715348b50ad8304357b43d61d09907e77fd7763e
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 20:38:03 +0100

    add swagger to auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,25 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiOperation,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { AuthenticationTokenDTO } from 'src/token/DTOs/authentication-token.dto';
 import { TokenService } from 'src/token/token.service';
 import { CurrentUser } from 'src/user/decorators/current-user.decorator';
 
+@ApiTags('auth')
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly tokenServcie: TokenService) {}
 
   @UseGuards(AuthGuard('local'))
+  @ApiOperation({ summary: 'Log in' })
+  @ApiOkResponse({ type: AuthenticationTokenDTO })
+  @ApiUnauthorizedResponse({ description: 'Wrong credentials' })
   @Post()
   async login(@CurrentUser() user: User): Promise<AuthenticationTokenDTO> {
     return this.tokenServcie.generateAuthenticationToken(user);
