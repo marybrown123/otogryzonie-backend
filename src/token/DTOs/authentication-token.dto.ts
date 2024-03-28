24294de686017fbddc3d969e854ff874d5780242
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 21:10:32 +0100

    add swagger to token

--- src/token/DTOs/authentication-token.dto.ts
+++ src/token/DTOs/authentication-token.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class AuthenticationTokenDTO {
+  @ApiProperty({ type: 'string', example: 'example.authentication.token' })
   authenticationToken: string;
 }
