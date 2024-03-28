6a2e68e8930a8ed2089d06d6b89259e0ce1de5fe
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 18:33:07 +0100

    update auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
+import { TokenModule } from 'src/token/token.module';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [UserModule, PassportModule, TokenModule],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
