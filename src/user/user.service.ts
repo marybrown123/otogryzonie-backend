220b9fd67fd6462104dd52d8c9ce6b878abbdb09
Maria Biss <mariabiss3012@gmail.com>
2024-03-28 13:12:56 +0100

    add create user and hash password methods in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDTO } from 'src/user/DTOs/create-user.dto';
+import { UserResponse } from 'src/user/responses/user.response';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(private prismaService: PrismaService) {}
+
+  async createUser(user: CreateUserDTO): Promise<UserResponse> {
+    const userFromDb = await this.prismaService.user.findUnique({
+      where: {
+        email: user.email,
+      },
+    });
+
+    if (userFromDb) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+
+    const hashedPassword = await this.hashPassword(user.password);
+
+    const userFroDb = await this.prismaService.user.create({
+      data: {
+        email: user.email,
+        password: hashedPassword,
+        name: user.name,
+        secondName: user.secondName,
+        phoneNumber: user.phoneNumber,
+        type: user.type,
+      },
+    });
+
+    return new UserResponse(userFroDb);
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
 }
