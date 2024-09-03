d0328ed3b6a28ed89788c285270a7492e90fb5d6
Marysia Biss <mariabiss3012@gmail.com>
2024-09-03 16:20:00 +0200

    fix advertisment response

--- src/advertisment/responses/advertisment.response.ts
+++ src/advertisment/responses/advertisment.response.ts
@@ -3,22 +3,28 @@ import {
   AdvertismentType,
   Features,
   Rodent,
+  User,
 } from '@prisma/client';
 import { RodentResponse } from 'src/rodent/responses/rodent.response';
+import { UserResponse } from 'src/user/responses/user.response';
 
 export class AdvertismentResponse {
   constructor(
-    advertisment: Advertisment & { rodent: Rodent & { features: Features } },
+    advertisment: Advertisment & { rodent: Rodent & { features: Features } } & {
+      user: User;
+    },
   ) {
     this.id = advertisment.id;
     this.description = advertisment.description;
     this.type = advertisment.type;
     this.location = advertisment.location;
     this.rodent = new RodentResponse(advertisment.rodent);
+    this.user = new UserResponse(advertisment.user);
   }
   id: number;
   description: string;
   type: AdvertismentType;
   location: string;
   rodent: RodentResponse;
+  user: UserResponse;
 }
