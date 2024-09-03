92693eb756176ea8176bd3178199dcb5243f7696
Marysia Biss <mariabiss3012@gmail.com>
2024-09-03 16:20:18 +0200

    fix rodent dto

--- src/rodent/DTOs/rodent.dto.ts
+++ src/rodent/DTOs/rodent.dto.ts
@@ -1,8 +1,8 @@
-import { RodentImage } from '@prisma/client';
+import { RodentType } from '@prisma/client';
 import { CreateFeaturesDTO } from 'src/features/DTOs/features.dto';
 
 export class CreateRodentDTO {
   name: string;
-  type: RodentImage;
+  type: RodentType;
   features: CreateFeaturesDTO;
 }
