d107846c162148d570215d6caac8eaf5f0ccd449
Maria Biss <mariabiss3012@gmail.com>
2025-01-10 12:31:56 +0100

    user controller @patch

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
@@ -6,6 +6,7 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { CreateUserDTO } from 'src/user/DTOs/create-user.dto';
+import { UpdateUserDTO } from 'src/user/DTOs/update-user.dto';
 import { UserResponse } from 'src/user/responses/user.response';
 import { UserService } from 'src/user/user.service';
 
@@ -20,5 +21,16 @@ export class UserController {
   @ApiBadRequestResponse({ description: 'Bad Request' })
   async createUser(@Body() user: CreateUserDTO): Promise<UserResponse> {
     return this.userService.createUser(user);
+  }
+
+  @Patch('/:id')
+  @ApiOperation({ summary: 'Update user' })
+  @ApiCreatedResponse({ type: UserResponse })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  async updateUser(
+    @Param('id') id: string,
+    @Body() newUser: UpdateUserDTO,
+  ): Promise<UserResponse> {
+    return this.userService.updateUser(Number(id), newUser);
   }
 }
